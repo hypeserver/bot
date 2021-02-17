fcea6f1826a86be345f1a0552d8e97043718b565
Yigit Guneli <yigiit@gmail.com>
2021-02-17 20:54:23 +0100

    move handler functions to app.py for now again, import fails because of decorators

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from slack_bolt import App
 
-from handlers import file_shared
+import image as im
 
 token = os.environ.get("SLACK_BOT_TOKEN")
 secret = os.environ.get("SLACK_SIGNING_SECRET")
@@ -11,6 +11,30 @@ app = App(
     signing_secret=secret
 )
 
+@app.event('file_shared')
+def file_shared(body, client, context, logger):
+    file_id = body["event"]["file_id"]
+    file_info = client.files_info(file=file_id)
+
+    if file_info['file']['filetype'] not in ['jpg']:
+        return
+
+    url = file_info['file']['url_private']
+
+    image = im.open_url(url, token)
+
+    mirrored = im.mirror(image)
+    mirrored.save('/tmp/%s.jpg'%file_id)
+
+    with open('/tmp/%s.jpg'%file_id, 'rb') as file_content:
+        response = client.files_upload(
+            file=file_content,
+            channels="sapsik"
+            )
+
+
 # Start your app
 if __name__ == "__main__":
     app.start(port=int(os.environ.get("PORT", 3000)))
+
+
